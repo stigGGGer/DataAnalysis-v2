3326b6e3fef1e56b1954b6f3a666b368e93d9446
veldm <Dmavel8@yandex.ru>
2022-06-26 16:39:44 +0300

    Фикс AP

--- DataAnalysis/clustering/Affinity_Propagation.py
+++ DataAnalysis/clustering/Affinity_Propagation.py
@@ -7,8 +7,8 @@ class Affinity(Enum):
     precomputed = "precomputed"
 
 def Affinity_Propagation(table, parametrs):
-    #clustering = AffinityPropagation(random_state = parametrs[1], affinity = parametrs[0], preference = parametrs[2], damping = parametrs[3])
-    clustering = AffinityPropagation(max_iter=500, random_state = parametrs[1], affinity = parametrs[0], damping = parametrs[2])
+    clustering = AffinityPropagation(random_state = parametrs[1], affinity = parametrs[0], preference = parametrs[2], damping = parametrs[3])
+    #clustering = AffinityPropagation(max_iter=500, random_state = parametrs[1], affinity = parametrs[0], damping = parametrs[2])
     Y_preds = clustering.fit_predict(table)
     #return [table, Y_preds]
     return table.assign(Y_Pred=Y_preds)
